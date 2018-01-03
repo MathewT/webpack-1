f314016ebcacfab38c565c43e3cb6bfb8b6f4687
Mathew Thomas <mathew.thomas@longdivision.com>
2018-01-02 19:54:43 -0800

    fixed image bug
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,9 +1,14 @@
 import '../styles/image_viewer.css';
-import '../images/sm-1.jpeg';
-import '../images/large-veggie.jpg';
-import '../images/small-kids.jpg';
+import sm1 from '../images/sm-1.jpeg';
+import lgveggie from '../images/large-veggie.jpg';
+import smkids from '../images/small-kids.jpg';
 
-const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+const sm1Image = document.createElement('img');
+sm1Image.src = sm1;
 
-document.body.appendChild(image);
+document.body.appendChild(sm1Image);
+
+const veggieImage = document.createElement('img');
+veggieImage.src = lgveggie;
+
+document.body.appendChild(veggieImage);
